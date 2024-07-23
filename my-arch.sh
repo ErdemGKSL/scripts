dccf7022bafc70dcd57105cc8e60a6f874e04bf7
Erdem <74366943+ErdemGKSL@users.noreply.github.com>
2024-07-23 11:22:17 +0300

    Update my-arch.sh

--- my-arch.sh
+++ my-arch.sh
@@ -1,5 +1,5 @@
 # Erdem's base arch setup (im tired of it :')
-sudo pacman -S --noconfirm floorp-bin
+sudo pacman -S --needed --noconfirm floorp-bin
 
 sudo pacman -S --needed --noconfirm git base-devel
 
@@ -13,27 +13,25 @@ makepkg -si
 
 cd ~/
 
-yay -S --noconfirm ttf-ms-win11-auto
-yay -S --noconfirm ttf-twemoji
+yay -S --needed --noconfirm ttf-ms-win11-auto
+yay -S --needed --noconfirm ttf-twemoji
 
 mkdir -p /etc/fonts/conf.d
 curl -L -o /etc/fonts/conf.d/99-emoji.conf https://raw.githubusercontent.com/ErdemGKSL/my-arch/main/assets/99-emoji.conf
 curl -L -o /etc/fonts/local.conf https://raw.githubusercontent.com/ErdemGKSL/my-arch/main/assets/local.conf
 sudo fc-cache -fv
 
-sudo pacman -S --noconfirm steam
-yay -S --noconfirm sunroof-bin
+sudo pacman -S --needed --noconfirm steam
+yay -S --needed --noconfirm sunroof-bin
 
 sudo pacman -S --noconfirm --needed curl
-sudo pacman -S --noconfirm dpkg
+sudo pacman -S --noconfirm --needed dpkg
 
 curl -L -o ~/Downloads/my-arch/resktop_1.5.4.deb https://github.com/Rivercord/Resktop/releases/download/v1.5.4/resktop_1.5.4_amd64.deb
 sudo dpkg -i ~/Downloads/my-arch/resktop_1.5.4.deb
 
-yay -S --noconfirm visual-studio-code-bin
+yay -S --noconfirm --needed visual-studio-code-bin
 
-sudo pacman -S --noconfirm jdk-openjdk
-sudo pacman -S --noconfirm jdk17-openjdk
-sudo pacman -S --noconfirm jdk11-openjdk
+sudo pacman -S --noconfirm --needed jdk-openjdk jdk17-openjdk jdk11-openjdk
 
-yay -S --noconfirm prismlauncher-bin
+yay -S --noconfirm --needed prismlauncher-bin
