ca81f97f54ae7b2c6af54cbb94afb5f28026bd92
Erdem <74366943+ErdemGKSL@users.noreply.github.com>
2026-05-09 12:16:59 +0000

    Simplify AUR setup and install yay directly
    Removed installation of yay from source and adjusted chaotic AUR installer usage.

--- my-arch.sh
+++ my-arch.sh
@@ -7,19 +7,14 @@ sudo pacman -S --needed --noconfirm reflector
 sudo reflector --country 'United Kingdom,Germany' --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syy
 
+wget -q -O chaotic-AUR-installer.bash https://raw.githubusercontent.com/SharafatKarim/chaotic-AUR-installer/main/install.bash && sudo bash chaotic-AUR-installer.bash && rm chaotic-AUR-installer.bash
+
 # setup yay
-mkdir ~/Documents/git
-cd ~/Documents/git
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
+sudo pacman -S yay
 
 # install usefull fonts
 yay -S --needed --noconfirm ttf-ms-win11-auto
 sudo pacman -S all-repository-fonts
-
-# install chaotic aur
-wget -q -O chaotic-AUR-installer.bash https://raw.githubusercontent.com/SharafatKarim/chaotic-AUR-installer/main/install.bash && sudo bash chaotic-AUR-installer.bash && rm chaotic-AUR-installer.bash
 
 # 32bit utility gpu modules, bc arch linux doesnt have it defaulitly (they are used to run 2d games or older games like portal 2 etc.)
 if lspci -nn | grep -E "VGA|3D|Display" | grep -qi "10de:"; then
